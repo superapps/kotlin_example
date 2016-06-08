05893b79f147ddc6dd7d185cd774e893e81ee57c
heeyoung.jeon <heeyoung.jeon@linecorp.com>
2016-06-08 15:18:06 +0900

    Remove ankle

--- Calculator/app/src/main/java/com/giocode/example/calculator/CalculatorActivity.kt
+++ Calculator/app/src/main/java/com/giocode/example/calculator/CalculatorActivity.kt
@@ -1,40 +1,35 @@
 package com.giocode.example.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
 import com.udojava.evalex.Expression
-import org.jetbrains.anko.*
 
 class CalculatorActivity : AppCompatActivity() {
 
     val calculator = Calculator()
-    var resultEdit: TextView? = null
+    val resultEdit: TextView by lazy {
+        findViewById(R.id.calculator_result) as TextView
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        consistView();
-    }
+        setContentView(R.layout.activity_calculator)
+        val edit = findViewById(R.id.calculator_edit) as EditText
+        edit.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                resultEdit.text = calculator.calculate(text.toString())
+            }
 
-    private fun consistView() {
-        verticalLayout {
-            padding = dip(16)
-            editText() {
-                textSize = sp(15).toFloat()
-                hint = "1+1/3"
-                textChangedListener {
-                    onTextChanged {
-                        text, start, before, count ->
-                        resultEdit?.text = calculator.calculate(text.toString())
-                    }
-                }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-            resultEdit = textView() {
-                textSize = sp(15).toFloat()
-                gravity = Gravity.RIGHT
+
+            override fun afterTextChanged(s: Editable?) {
             }
-        }
+        })
     }
 
     inner class Calculator {
