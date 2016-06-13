46bba3f9a02d403b59ae1eeafab5dffd11de93a8
hyeyoung kwon <hyeyoung.kwon@linecorp.com>
2016-06-13 20:12:35 +0900

    Modify code to use delegator

--- memo/app/src/main/java/com/hyeyoung/memo/MemoDao.kt
+++ memo/app/src/main/java/com/hyeyoung/memo/MemoDao.kt
@@ -1,20 +1,46 @@
 package com.hyeyoung.memo
 
 import java.util.*
+import kotlin.properties.Delegates
 
-class MemoDao {
+class MemoDao() {
+
+    var onDataChangeListener: OnDataChageListener? = null
+
+    var size: Int by Delegates.observable(Int.MIN_VALUE) {
+        d, old, new ->
+        onDataChangeListener?.onDataChanged()
+    }
+
     val list = ArrayList<Memo>()
 
-    fun add(memo: Memo) = list.add(memo)
 
-    fun remove(position: Int) = list.remove(list[position])
+    fun add(memo: Memo) {
+        list.add(memo)
+        size = list.size
+    }
+
+    fun remove(position: Int) {
+        list.remove(list[position])
+        size = list.size
+    }
 
     // TODO
     //    fun getRecentMemoList(createdTime: Long) : List<Memo> = list.filter {
     //
     //    }
 
-    fun sortByTitle() = list.sortBy { it.title }
+    fun sortByTitle() {
+        list.sortBy { it.title }
+        size = list.size
+    }
 
-    fun sortByCreatedTime() = list.sortBy { it.createdTime }
+    fun sortByCreatedTime() {
+        list.sortBy { it.createdTime }
+        size = list.size
+    }
+
+    interface OnDataChageListener {
+        fun onDataChanged()
+    }
 }
